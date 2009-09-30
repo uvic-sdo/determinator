ac0f843bc5d7480f6ed152059f081bd5dd36ce14
Stephen Sugden <grncdr@uvic.ca>
2009-09-29 20:40:07 -0700

    Added filename parsing
    Implemented rudimentary filename parsing, details in
    filename-patterns.txt

--- determinator.py
+++ determinator.py
@@ -6,11 +6,12 @@ mimetypes.init()
 mimetypes.add_type('audio/flac','.flac')
 mimetypes.add_type('video/matroska','.mkv')
 mimetypes.add_type('video/avi','.xvid')
+mimetypes.add_type('video/wmv','.wmv')
 
 def match_mime(pattern, type):
     pattern = pattern.split('/')
     type = type[0].split('/')
-    for i in [1,2]:
+    for i in range( min( len(pattern), len(type) ) ):
         if pattern[i] == '*' or pattern[i] == type[i]:
             continue
         else:
@@ -20,24 +21,35 @@ def match_mime(pattern, type):
 try:
     filename = sys.argv[1]
 except:
-    print "No filename provided"
-    return
+    print("No filename provided")
+    quit()
 
 if os.path.isfile(filename): filename = os.path.abspath(filename)
 
+#FIXME - This will be a config variable later
+root = os.getcwd()
+
 type = mimetypes.guess_type(filename)
-if type[0].split('/')[0] not in ['audio', 'video']:
-    print "Unsupported file type"
-    return
+if not type[0] or type[0].split('/')[0] not in ['audio', 'video']:
+    print("Unsupported file type")
+    quit()
 
 fh = open('filename-patterns.txt')
+metadata = {}
 for line in fh:
-    mime_pattern, regex = line.split('\t')
-    if match_mime(mime_pattern, regex):
+    if line[0] == '#' or line == '\n':
+        continue
+    try:
+        mime_pattern, regex = line.split('\t')
+        regex = regex.strip()
+    except ValueError:
+        print("Invalid filename pattern line:", line)
+        quit()
+    if match_mime(mime_pattern, type):
         regex = re.compile(regex)
-        match = regex.search(filename)
+        match = regex.search( filename.replace(root,'') )
         if match:
             metadata = match.groupdict()
             break
 
-print filename, metadata 
+print(filename, metadata)
