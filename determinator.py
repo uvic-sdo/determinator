59478ff47edcf83e2be4e84cb9fafba4adcd3c72
Stephen Sugden <grncdr@uvic.ca>
2009-10-02 13:28:56 -0700

    Fixed mime-types, further divided some objects

--- determinator.py
+++ determinator.py
@@ -1,29 +1,26 @@
 import os.path
 import sys
-import mimetypes
+import mimetypes 
 import re
+import fnmatch
 #import configparser
-mimetypes.init('mime.types')
 
 class SourceFile(object):
     ''' This class represents a file to be determinated. It requires a valid 
     pathname and an FilenameParser object '''
 
     def __init__(self, filename, fnparser):
-        '''
-        '''        
         if not os.path.isfile(filename):
             raise IOError("Could not read file: '"+filename+"'")
         self.fnparser = fnparser
         self.filename = os.path.abspath(filename)
         self.type = mimetypes.guess_type(filename)
         if not self.type[0] or self.type[0].split('/')[0] not in ['audio', 'video']:
-            raise IOError("Unsupported filetype in file '"+filename+"', type: '"+self.type[0]+"'")
+            raise IOError("Unsupported filetype in file '"+filename+"'")
         self.metadata = {} # It might be wise to have globally available metadata defined here, e.g. 'video_root' defining a base directory for all videos
 
     def getMetadata(self):
-        ''' Extract metadata by calling FileParser.parse, and any other valid
-        extraction method '''
+        ''' Extract metadata by calling FileParser.parse, and any other valid extraction method '''
         self.metadata = self.fnparser.parse(self)
         if self.type[0] == 'audio/mpeg':
             print 'Extracting ID3 tags not implemented yet'
@@ -52,23 +49,24 @@ class SourceFile(object):
 
 class FilenameParser(object):
     def __init__(self, pattern_file, root=None):
-        file = open(pattern_file, 'r')
-        self.patterns = [self.readline(line) for line in real_lines(file) if line != None]
-        file.close()
+        self.file = open(pattern_file, 'r')
         self.root = root or ''
 
     def readline(self, line):
         try: mime_pattern, regex = re.split('\t+', line)
         except: return None
-        regex = regex.strip()
-        del line
-        return locals()
+        regex = regex.strip().replace('/',os.path.sep)
+        return (mime_pattern, regex)
 
     def parse(self, sf):
         ''' Parse the given filename using filename patterns read in from rules file '''
-        for p in self.patterns:
-            if mime_match(p['mime_pattern'],type):
-                regex = re.compile(p['regex'])
+        self.file.seek(0)
+        for line in self.file:
+            if not comment(line): continue
+            line = self.readline(line)
+            print line
+            if line and match_mime(line[0], sf.type):
+                regex = re.compile(line[1])
                 match = regex.search( sf.filename.replace(self.root,'') )
                 if match:
                     return match.groupdict()
@@ -76,32 +74,29 @@ class FilenameParser(object):
 
 class TargetRuleSet(object):
     def __init__(self, filename):
-        file = open(filename, 'r')
-        self.patterns = [self.readline(line) for line in real_lines(file) if line != None]
-        file.close()
+        self.file = open(filename, 'r')
         
     def readline(self, line):
         try: mime_pattern, target, operators = re.split('\t\s*', line)
         except: return None
-        del line
-        return locals()
+        return (mime_pattern, target, operators)
 
-    def getRules(self, metadata):
+    def getRules(self, sf):
         ''' Find matching rules for the given metadata in the target rules file '''
+        self.file.seek(0)
         rules = []
-        for p in self.patterns:
-            if match_mime(p['mime_pattern'], type):
-                try: target = p['target'].format(**metadata)
+        for line in self.file:
+            if not comment(line): continue
+            line = self.readline(line)
+            if line and match_mime(line[0], sf.type):
+                try: target = line[1].format(**sf.metadata)
                 except KeyError, IndexError: continue
-                rules.append((target, p['operators']))
-                if p['operators'].find('f') < 0: break
+                rules.append((target, line[2]))
+                if line[2].find('f') < 0: break
         return rules
 
 def match_mime(pattern, type):
-    return bool( re.match(fnmatch.translate(pattern), type[0]))
-
-def real_lines(list):
-    return [line.strip() for line in list if not comment(line)]
+    return bool( re.match( fnmatch.translate( pattern ), type[0] ) )
 
 def comment(line):
     if line == '' or line[0] == '#': return True
@@ -115,6 +110,7 @@ if __name__ == "__main__":
         print("No filename provided")
         quit()
 
+    mimetypes.init(['mime.types'])
     fnparser = FilenameParser('filename-patterns.txt', root)
     rules = TargetRuleSet('match-rules.txt')
 
@@ -122,4 +118,4 @@ if __name__ == "__main__":
         linking_disabled = True
     sourcefile = SourceFile(filename, fnparser)
     sourcefile.getMetadata()
-    sourcefile.applyRules( rules.getRules( sourcefile.metadata ) )
+    sourcefile.applyRules( rules.getRules( sourcefile ) )
